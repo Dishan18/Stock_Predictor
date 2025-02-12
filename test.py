17e48616aa4495b27afaa12f54fe09bb8451cc5f
SaharshDaNerd <saharshcodes@gmail.com>
2025-02-12 20:57:04 +0530

    made a web scrapper to get some news about a stock

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("Hello world")
+from bs4 import BeautifulSoup
+from urllib.request import urlopen, Request
+
+tickertape_url = "https://www.tickertape.in/stocks/"
+tickers = ['PNBK','MRF','RAIV','NTPC']
+news_headlines = {}
+
+for ticker in tickers:
+    url = tickertape_url + ticker
+    req = Request(url=url, headers={'user-agent': 'my-app'})
+    response = urlopen(req)
+    html = BeautifulSoup(response, 'html.parser')
+
+    news_tab = html.find(id='news-tab')
+    if news_tab:
+        headlines = [headline.text for headline in news_tab.find_all(
+            'span', class_='jsx-1669302504 text-primary headline mb8 heading')]
+        news_headlines[ticker] = headlines
+
+print(news_headlines)
